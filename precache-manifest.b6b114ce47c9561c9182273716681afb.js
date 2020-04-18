b121989a8c67998bd7a33049f7ca8c584ebfd639
Rajat <rbudania99@gmail.com>
2020-04-19 02:05:53 +0530

    Updates

---
+++ precache-manifest.b6b114ce47c9561c9182273716681afb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6270255cd1e4dd6473da96579b1d0ed4",
+    "url": "/CovidTracker/index.html"
+  },
+  {
+    "revision": "1a3f5ca3ed0158691033",
+    "url": "/CovidTracker/static/css/main.cec3a0f2.chunk.css"
+  },
+  {
+    "revision": "3f36be100aa22dfe4a7a",
+    "url": "/CovidTracker/static/js/2.e6c3ee95.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/CovidTracker/static/js/2.e6c3ee95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a3f5ca3ed0158691033",
+    "url": "/CovidTracker/static/js/main.abd6b232.chunk.js"
+  },
+  {
+    "revision": "f24a48d69bf495da1ca8",
+    "url": "/CovidTracker/static/js/runtime-main.88d0797f.js"
+  }
+]);
